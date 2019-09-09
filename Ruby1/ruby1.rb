0ed7d99546bd76cdda2905630a5880936d12b8a3
Dmitry Serov <barbari100@gmail.com>
2019-09-09 19:42:40 +0700

    Add sorting, remove duplicates

--- Ruby1/ruby1.rb
+++ Ruby1/ruby1.rb
@@ -1,4 +1,12 @@
+require 'set'
+
 def words_with_symbols(symbols_list, n)
+  unique_symbols_list = symbols_list.to_set
+  words_with_symbols_internal(unique_symbols_list, n)
+    .sort
+end
+
+def words_with_symbols_internal(symbols_list, n)
   (1..n).reduce(['']) do |prev_list, _|
     symbols_list.map { |symbol|
       prev_list.reject { |prev_word| prev_word.end_with?(symbol) }
