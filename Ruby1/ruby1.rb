4fccae3b1f04ee7345efaa4c1b0586975f856319
Dmitry Serov <barbari100@gmail.com>
2019-09-10 16:59:08 +0700

    Adhere to airbnb's Ruby style guide

--- Ruby1/ruby1.rb
+++ Ruby1/ruby1.rb
@@ -1,18 +1,18 @@
 require 'set'
 
-def words_with_symbols(symbols_list, n)
-  unique_symbols_list = symbols_list.to_set
-  words_with_symbols_internal(unique_symbols_list, n)
-    .sort
+def words_with_characters(characters_list, word_length)
+  unique_characters_list = characters_list.to_set
+  words_with_characters_internal(unique_characters_list, word_length).sort
 end
 
-def words_with_symbols_internal(symbols_list, n)
-  (1..n).reduce(['']) do |prev_list, _|
-    symbols_list.map { |symbol|
-      prev_list.reject { |prev_word| prev_word.end_with?(symbol) }
-               .map { |prev_word| prev_word + symbol }
-    }.flatten
+def words_with_characters_internal(characters_list, word_length)
+  (1..word_length).reduce(['']) do |words, _|
+    words_with_added_characters = characters_list.map do |character|
+      words.reject { |word| word.end_with?(character) }.map { |word| "#{word}#{character}" }
+    end
+
+    words_with_added_characters.flatten
   end
 end
 
-puts words_with_symbols(%w[a b c], 4)
+puts words_with_characters(%w(a b c), 4)
