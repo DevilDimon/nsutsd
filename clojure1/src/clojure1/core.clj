bc5db4f570346dc6ebb755a1295de276231392ae
Dmitry Serov <barbari100@gmail.com>
2019-10-13 14:28:17 +0700

    Refactor Clojure1 a bit

--- clojure1/src/clojure1/core.clj
+++ clojure1/src/clojure1/core.clj
@@ -1,15 +1,17 @@
 (ns clojure1.core)
 
+(defn build-words [words character]
+  (map (fn [word] (.concat word character))
+    (remove (fn [word] (.endsWith word character))
+      words)))
+
 (defn words-with-characters [characters word-length]
   (reduce (fn [words _]
     (flatten
-     (map (fn [character]
-       (map (fn [word] (.concat word character))
-            (remove (fn [word] (.endsWith word character))
-              words)))
-          characters)))
-          '("")
-          (range 1 (inc word-length))))
+     (map (fn [character] (build-words words character))
+       characters)))
+       '("")
+       (range 1 (inc word-length))))
 
 (defn -main [& args]
   (println (words-with-characters '("a" "b" "c") 4)))
