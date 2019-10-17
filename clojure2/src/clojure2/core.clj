b40fbda23d3f8faff008eca0a7f4dbf025c69949
Dmitry Serov <barbari100@gmail.com>
2019-10-17 11:15:05 +0700

    Add Clojure2 memoization

--- clojure2/src/clojure2/core.clj
+++ clojure2/src/clojure2/core.clj
@@ -5,12 +5,14 @@
 (defn f-i [f x i]
   (f (* (/ x interval-count) i)))
 
+(def f-i-memoized (memoize f-i))
+
 (defn integrate [f x]
   (* (/ x interval-count)
      (+ (reduce +
-                (map (fn [i] (f-i f x i))
+                (map (fn [i] (f-i-memoized f x i))
                      (range 1 interval-count)))
-        (/ (+ (f-i f x interval-count) (f-i f x 0)) 2))))
+        (/ (+ (f-i-memoized f x interval-count) (f-i-memoized f x 0)) 2))))
 
 (defn -main [& args]
   (println (integrate (fn [x] (* x x)) 5)))
